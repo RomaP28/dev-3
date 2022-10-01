0f3788cd5c819e5d4d3df6a887e2995f2f4550cd
Roman Posokhov <posokhovrom@gmail.com>
2022-10-01 23:39:13 +0300

    remove backdrop-filter

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 
 
 const header = document.querySelector('.header');
-// const headerHeight = header.getBoundingClientRect().height
+const headerHeight = header.getBoundingClientRect().height
 
 document.querySelectorAll('.nav li a').forEach(elem=>elem.addEventListener('click', e => {
     e.preventDefault()
@@ -102,84 +102,83 @@ bgToAnimate.forEach(el => gsap.to(el.selector, {
 //
 // document.addEventListener('DOMContentLoaded', mainAnimation);
 
-// window.onload = typingEffect;
-//
-// function typingEffect() {
-//     let intervalId;
-//
-//     function pendingStart() {
-//         document.querySelector('.title').innerHTML = `Global software development firm with <span class="violet">100% <span class="text">reliability </span></span><span class="cursor"></span>rate`
-//         intervalId = setInterval(deleteWords,60)
-//     }
-//
-//     let timerId = setTimeout(pendingStart, 1200);
-//
-//     const deleteWords = () => {
-//         clearTimeout(timerId);
-//         let text = document.querySelector('.text').innerHTML;
-//         document.querySelector('.cursor').innerHTML = '|';
-//         document.querySelector('.text').innerHTML = text.slice(0, -1);
-//         if (text.length === 0) {
-//             clearInterval(intervalId)
-//             startType()
-//         }
-//     }
-// }
-//
-// const startType = () => {
-//     const rever = () => masterTl.timeScale(4);
-//     const start = () => masterTl.timeScale(1)
-//     let masterTl = gsap.timeline({repeat: -1, yoyo: true, repeatDelay: 3});
-//     let tl = gsap.timeline();
-//     tl.to(".text", {duration: 2, delay: 0, ease: "none" ,text: 'reliability', onComplete: () => rever(), onStart: () => start() });
-//     masterTl.add(tl);
-// }
+window.onload = typingEffect;
+
+function typingEffect() {
+    let intervalId;
+
+    function pendingStart() {
+        document.querySelector('.title').innerHTML = `Global software development firm with <span class="violet">100% <span class="text">reliability </span></span><span class="cursor"></span>rate`
+        intervalId = setInterval(deleteWords,60)
+    }
+
+    let timerId = setTimeout(pendingStart, 1200);
+
+    const deleteWords = () => {
+        clearTimeout(timerId);
+        let text = document.querySelector('.text').innerHTML;
+        document.querySelector('.cursor').innerHTML = '|';
+        document.querySelector('.text').innerHTML = text.slice(0, -1);
+        if (text.length === 0) {
+            clearInterval(intervalId)
+            startType()
+        }
+    }
+}
+
+const startType = () => {
+    console.log('asd')
+    const rever = () => masterTl.timeScale(4);
+    const start = () => masterTl.timeScale(1)
+    let masterTl = gsap.timeline({repeat: -1, yoyo: true, repeatDelay: 3});
+    let tl = gsap.timeline();
+    tl.to(".text", {duration: 2, delay: 0, ease: "none" ,text: 'reliability', onComplete: () => rever(), onStart: () => start() });
+    masterTl.add(tl);
+}
 
 
+gsap.to('.sticky', {
+    y: 0,
+    opacity: 1,
+    duration: 1,
+    scrollTrigger: {
+        trigger: '.header .wrapper',
+        start: "bottom 20%",
+        end: 'bottom 20%',
+        toggleActions: "play none none reverse"
+    }
+});
 
 
-// gsap.to('.sticky', {
-//     y: 0,
-//     opacity: 1,
-//     duration: 1,
-//     scrollTrigger: {
-//         trigger: '.header .wrapper',
-//         start: "bottom 20%",
-//         end: 'bottom 20%',
-//         toggleActions: "play none none reverse"
-//     }
-// });
+const elementsToAnimate = [
+    {selector: '.work .h-primary', trigger: '.work', position: 'top 70%', effect: 'back'},
+    // {selector: '.work .paragraph', trigger: '.work .paragraph', position: 'bottom 80%', effect: 'circ'},
+    {selector: '.rewards .h-primary', trigger: '.rewards', position: 'top 70%', effect: 'back'},
+    {selector: '.rewards .paragraph', trigger: '.rewards', position: 'top 60%', effect: 'circ'},
+    {selector: '.capabilities .h-primary', trigger: '.capabilities', position: 'top 70%', effect: 'back'},
+    {selector: '.capabilities .paragraph', trigger: '.capabilities', position: 'top 60%', effect: 'circ'},
+    {selector: '.approach .h-primary', trigger: '.approach', position: 'top 70%', effect: 'back'},
+    {selector: '.approach .paragraph', trigger: '.approach', position: 'top 60%', effect: 'circ'},
+    {selector: '.reviews .h-primary', trigger: '.reviews', position: 'top 70%', effect: 'back'},
+    {selector: '.reviews .paragraph', trigger: '.reviews', position: 'top 60%', effect: 'circ'},
+    {selector: '.contact-us .h-primary', trigger: '.contact-us', position: 'top 70%', effect: 'back'},
+    {selector: '.contact-us .paragraph', trigger: '.contact-us', position: 'top 60%', effect: 'circ'},
+]
 
-
-// const elementsToAnimate = [
-//     {selector: '.work .h-primary', trigger: '.work', position: 'top 70%', effect: 'back'},
-//     // {selector: '.work .paragraph', trigger: '.work .paragraph', position: 'bottom 80%', effect: 'circ'},
-//     {selector: '.rewards .h-primary', trigger: '.rewards', position: 'top 70%', effect: 'back'},
-//     {selector: '.rewards .paragraph', trigger: '.rewards', position: 'top 60%', effect: 'circ'},
-//     {selector: '.capabilities .h-primary', trigger: '.capabilities', position: 'top 70%', effect: 'back'},
-//     {selector: '.capabilities .paragraph', trigger: '.capabilities', position: 'top 60%', effect: 'circ'},
-//     {selector: '.approach .h-primary', trigger: '.approach', position: 'top 70%', effect: 'back'},
-//     {selector: '.approach .paragraph', trigger: '.approach', position: 'top 60%', effect: 'circ'},
-//     {selector: '.reviews .h-primary', trigger: '.reviews', position: 'top 70%', effect: 'back'},
-//     {selector: '.reviews .paragraph', trigger: '.reviews', position: 'top 60%', effect: 'circ'},
-//     {selector: '.contact-us .h-primary', trigger: '.contact-us', position: 'top 70%', effect: 'back'},
-//     {selector: '.contact-us .paragraph', trigger: '.contact-us', position: 'top 60%', effect: 'circ'},
-// ]
-
-// elementsToAnimate.forEach(el => gsap.to(el.selector, {
-//     y: 0,
-//     opacity: 1,
-//     duration: 0.7,
-//     ease: el.effect,
-//     repeat: 0,
-//     scrollTrigger: {
-//         // markers: true,
-//         trigger: el.trigger,
-//         start: el.position,
-//         end: 'top -30%',
-//         toggleActions: "restart reverse play reverse",
-//     }
-// }));
+elementsToAnimate.forEach(el => gsap.to(el.selector, {
+    y: 0,
+    opacity: 1,
+    duration: 0.7,
+    ease: el.effect,
+    repeat: 0,
+    scrollTrigger: {
+        // markers: true,
+        trigger: el.trigger,
+        start: el.position,
+        end: 'top -30%',
+        toggleActions: "restart reverse play reverse",
+    }
+}));
 
 const swiper = new Swiper('.swiper', {
     loop: true,
